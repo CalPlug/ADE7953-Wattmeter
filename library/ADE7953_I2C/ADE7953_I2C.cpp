ce843f10d3a0b1326b54b2bc73f58f3021866bc3
Michael John Klopfer <mklopfer@uci.edu>
2018-12-04 07:31:18 -0800

    Updates

--- library/ADE7953_I2C/ADE7953_I2C.cpp
+++ library/ADE7953_I2C/ADE7953_I2C.cpp
@@ -204,6 +204,267 @@ int ADE_Address = 56;//I2C Address of ADE7953, or using equivalent value in hex:
 #define LAST_RWDATA_24 0x2FF //LAST_RWDATA, (R) Default: 0x000000, Unsigned, Contains the data from the last successful 24-bit/32-bit register communication(24 bit)
 #define LAST_RWDATA_32 0x3FF //LAST_RWDATA, (R) Default: 0x000000, Unsigned, Contains the data from the last successful 24-bit/32-bit register communication(32 bit)
 
+//*****************
+/*
+ADE7953 REGISTER DESCRIPTIONS
+
+DISNOLOAD Register (Address 0x001)
+Bits Bit Name Default Description
+0 DIS_APNLOAD 0 1 = disable the active power no-load feature on Current Channel A and Current Channel B
+1 DIS_VARNLOAD 0 1 = disable the reactive power no-load feature on Current Channel A and Current Channel B
+2 DIS_VANLOAD 0 1 = disable the apparent power no-load feature on Current Channel A and Current Channel B
+
+LCYCMODE Register (Address 0x004)
+Bits Bit Name Default Description
+0 ALWATT 0 0 = disable active energy line cycle accumulation mode on Current Channel A
+1 = enable active energy line cycle accumulation mode on Current Channel A
+1 BLWATT 0 0 = disable active energy line cycle accumulation mode on Current Channel B
+1 = enable active energy line cycle accumulation mode on Current Channel B
+2 ALVAR 0 0 = disable reactive energy line cycle accumulation mode on Current Channel A
+1 = enable reactive energy line cycle accumulation mode on Current Channel A
+3 BLVAR 0 0 = disable reactive energy line cycle accumulation mode on Current Channel B
+1 = enable reactive energy line cycle accumulation mode on Current Channel B
+4 ALVA 0 0 = disable apparent energy line cycle accumulation mode on Current Channel A
+1 = enable apparent energy line cycle accumulation mode on Current Channel A
+5 BLVA 0 0 = disable apparent energy line cycle accumulation mode on Current Channel B
+1 = enable apparent energy line cycle accumulation mode on Current Channel B
+6 RSTREAD 1 0 = disable read with reset for all registers
+1 = enable read with reset for all registers
+
+CONFIG Register (Address 0x102)
+Bits Bit Name Default Description
+0 INTENA 0 1 = integrator enable (Current Channel A)
+1 INTENB 0 1 = integrator enable (Current Channel B)
+2 HPFEN 1 1 = HPF enable (all channels)
+3 PFMODE 0 0 = power factor is based on instantaneous powers, 1 = power factor is based on line cycle accumulation mode energies
+4 REVP_CF 0 0 = REVP is updated on CF1, 1 = REVP is updated on CF2
+5 REVP_PULSE 0 0 = REVP is high when reverse polarity is true, low when reverse polarity is false, 1 = REVP outputs a 1 Hz pulse when reverse polarity is true and is low when reverse polarity is false
+6 ZXLPF 0 0 = ZX LPF is enabled, 1 = ZX LPF is disabled
+7 SWRST 0 Setting this bit enables a software reset
+8 CRC_ENABLE 0 0 = CRC is disabled, 1 = CRC is enabled
+[10:9] Reserved 00 Reserved
+11 ZX_I 0 0 = ZX_I is based on Current Channel A, 1 = ZX_I is based on Current Channel B
+[13:12] ZX_EDGE 00 Zero-crossing interrupt edge selection
+Setting               Edge Selection
+00                    Interrupt is issued on both positive-going and negative-going zero crossing
+01                    Interrupt is issued on negative-going zero crossing
+10                    Interrupt is issued on positive-going zero crossing
+11                    Interrupt is issued on both positive-going and negative-going zero crossing
+14                    Reserved 0 Reserved
+15                    COMM_LOCK 1 0 = communication locking feature is enabled, 1 = communication locking feature is disabled
+
+CFMODE Register (Address 0x107)
+Bits Bit Name Default Description
+[3:0] CF1SEL 0000 Configuration of output signal on CF1 pin
+Setting CF1 Output Signal Configuration
+0000 CF1 is proportional to active power (Current Channel A)
+0001 CF1 is proportional to reactive power (Current Channel A)
+0010 CF1 is proportional to apparent power (Current Channel A)
+0011 CF1 is proportional to IRMS (Current Channel A)
+0100 CF1 is proportional to active power (Current Channel B)
+0101 CF1 is proportional to reactive power (Current Channel B)
+0110 CF1 is proportional to apparent power (Current Channel B)
+0111 CF1 is proportional to IRMS (Current Channel B)
+1000 CF1 is proportional to IRMS (Current Channel A) + IRMS (Current Channel B)
+1001 CF1 is proportional to active power (Current Channel A) + active power (Current Channel B)
+[7:4] CF2SEL 0000 Configuration of output signal on CF2 pin 
+Setting              CF2 Output Signal Configuration
+0000                 CF2 is proportional to active power (Current Channel A)
+0001                 CF2 is proportional to reactive power (Current Channel A)
+0010                 CF2 is proportional to apparent power (Current Channel A)
+0011                 CF2 is proportional to IRMS (Current Channel A)
+0100                 CF2 is proportional to active power (Current Channel B)
+0101                 CF2 is proportional to reactive power (Current Channel B)
+0110                 CF2 is proportional to apparent power (Current Channel B)
+0111                 CF2 is proportional to IRMS (Current Channel B)
+1000                 CF2 is proportional to IRMS (Current Channel A) + IRMS (Current Channel B)
+1001                 CF2 is proportional to active power (Current Channel A) + active power(Current Channel B)
+8 CF1DIS 1     0 = CF1 output is enabled, 1 = CF1 output is disabled
+9 CF2DIS 1     0 = CF2 output is enabled, 1 = CF2 output is disabled
+
+ALT_OUTPUT Register (Address 0x110)
+Bits Bit Name Default Description
+[3:0] ZX_ALT 0000 Configuration of ZX pin (Pin 1)
+Setting 		ZX Pin Configuration
+0000 			ZX detection is output on Pin 1 (default)
+0001 			Sag detection is output on Pin 1
+0010 			Reserved
+0011 			Reserved
+0100 			Reserved
+0101 			Active power no-load detection (Current Channel A) is output on Pin 1
+0110 			Active power no-load detection (Current Channel B) is output on Pin 1
+0111 			Reactive power no-load detection (Current Channel A) is output on Pin 1
+1000 			Reactive power no-load detection (Current Channel B) is output on Pin 1
+1001 			Unlatched waveform sampling signal is output on Pin 1
+1010 			IRQ signal is output on Pin 1
+1011 			ZX_I detection is output on Pin 1
+1100 			REVP detection is output on Pin 1
+1101 			Reserved (set to default value)
+111x 			Reserved (set to default value)
+[7:4] ZXI_ALT 0000 Configuration of ZX_I pin (Pin 21)
+Setting 		ZX_I Pin Configuration
+0000 			ZX_I detection is output on Pin 21 (default)
+0001 			Sag detection is output on Pin 21
+0010 			Reserved
+0011 			Reserved
+0100 			Reserved
+0101 			Active power no-load detection (Current Channel A) is output on Pin 21
+0110 			Active power no-load detection (Current Channel B) is output on Pin 21
+0111 			Reactive power no-load detection (Current Channel A) is output on Pin 21
+1000 			Reactive power no-load detection (Current Channel B) is output on Pin 21
+1001 			Unlatched waveform sampling signal is output on Pin 21
+1010 			IRQ signal is output on Pin 21
+1011 			ZX detection is output on Pin 21
+1100 			REVP detection is output on Pin 21
+1101 			Reserved (set to default value)
+111x 			Reserved (set to default value)
+
+[11:8] REVP_ALT 0000 Configuration of REVP pin (Pin 20)
+Setting			REVP Pin Configuration
+0000 			REVP detection is output on Pin 20 (default)
+0001 			Sag detection is output on Pin 20
+0010 			Reserved
+0011 			Reserved
+0100 			Reserved
+0101 			Active power no-load detection (Current Channel A) is output on Pin 20
+0110 			Active power no-load detection (Current Channel B) is output on Pin 20
+0111 			Reactive power no-load detection (Current Channel A) is output on Pin 20
+1000 			Reactive power no-load detection (Current Channel B) is output on Pin 20
+1001 			Unlatched waveform sampling signal is output on Pin 20
+1010 			IRQ signal is output on Pin 20
+1011 			ZX detection is output on Pin 20
+1100 			ZX_I detection is output on Pin 20
+1101 			Reserved (set to default value)
+111x 			Reserved (set to default value)
+
+ACCMODE Register (Address 0x201 and Address 0x301)
+Bits Bit Name Default Description
+[1:0] AWATTACC 00 Current Channel A active energy accumulation mode
+Setting Active Energy Accumulation Mode (Current Channel A)
+	00 Normal mode
+	01 Positive-only accumulation mode
+	10 Absolute accumulation mode
+	11 Reserved
+[3:2] BWATTACC 00 Current Channel B active energy accumulation mode
+Setting Active Energy Accumulation Mode (Current Channel B)
+	00 Normal mode
+	01 Positive-only accumulation mode
+	10 Absolute accumulation mode
+	11 Reserved
+[5:4] AVARACC 00 Current Channel A reactive energy accumulation mode
+Setting Reactive Energy Accumulation Mode (Current Channel A)
+	00 Normal mode
+	01 Antitamper accumulation mode
+	10 Absolute accumulation mode
+	11 Reserved
+[7:6] BVARACC 00 Current Channel B reactive energy accumulation mode
+Setting Reactive Energy Accumulation Mode (Current Channel B)
+	00 Normal mode
+	01 Antitamper accumulation mode
+	10 Absolute accumulation mode
+	11 Reserved
+8 AVAACC 0 0 = Current Channel A apparent energy accumulation is in normal mode, 1 = Current Channel A apparent energy accumulation is based on IRMSA
+9 BVAACC 0 0 = Current Channel B apparent energy accumulation is in normal mode, 1 = Current Channel B apparent energy accumulation is based on IRMSB
+10 APSIGN_A 0 0 = active power on Current Channel A is positive, 1 = active power on Current Channel A is negative
+11 APSIGN_B 0 0 = active power on Current Channel B is positive, 1 = active power on Current Channel B is negative
+12 VARSIGN_A 0 0 = reactive power on Current Channel A is positive, 1 = reactive power on Current Channel A is negative
+13 VARSIGN_B 0 0 = reactive power on Current Channel B is positive, 1 = reactive power on Current Channel B is negative
+[15:14] Reserved 00 Reserved
+16 ACTNLOAD_A 0 0 = Current Channel A active energy is out of no-load condition, 1 = Current Channel A active energy is in no-load condition
+17 VANLOAD_A 0 0 = Current Channel A apparent energy is out of no-load condition, 1 = Current Channel A apparent energy is in no-load condition
+18 VARNLOAD_A 0 0 = Current Channel A reactive energy is out of no-load condition, 1 = Current Channel A reactive energy is in no-load condition
+19 ACTNLOAD_B 0 0 = Current Channel B active energy is out of no-load condition, 1 = Current Channel B active energy is in no-load condition
+20 VANLOAD_B 0 0 = Current Channel B apparent energy is out of no-load condition, 1 = Current Channel B apparent energy is in no-load condition
+21 VARNLOAD_B 0 0 = Current Channel B reactive energy is out of no-load condition, 1 = Current Channel B reactive energy is in no-load condition
+
+*/
+
+//Interrupt Configuration
+//Note: For IRQENA Register (Address 0x22C and Address 0x32C)), each binary position of the 16- bit response has the following configuration options
+//Bits Bit Name Description
+//0 AEHFA Set to 1 to enable an interrupt when the active energy is half full (Current Channel A)
+//1 VAREHFA Set to 1 to enable an interrupt when the reactive energy is half full (Current Channel A)
+//2 VAEHFA Set to 1 to enable an interrupt when the apparent energy is half full (Current Channel A)
+//3 AEOFA Set to 1 to enable an interrupt when the active energy has overflowed or underflowed (Current Channel A)
+//4 VAREOFA Set to 1 to enable an interrupt when the reactive energy has overflowed or underflowed (Current Channel A)
+//5 VAEOFA Set to 1 to enable an interrupt when the apparent energy has overflowed or underflowed (Current Channel A)
+//6 AP_NOLOADA Set to 1 to enable an interrupt when the active power no-load condition is detected on Current Channel A
+//7 VAR_NOLOADA Set to 1 to enable an interrupt when the reactive power no-load condition is detected on Current Channel A
+//8 VA_NOLOADA Set to 1 to enable an interrupt when the apparent power no-load condition is detected on Current Channel A
+//9 APSIGN_A Set to 1 to enable an interrupt when the sign of active energy has changed (Current Channel A)
+//10 VARSIGN_A Set to 1 to enable an interrupt when the sign of reactive energy has changed (Current Channel A)
+//11 ZXTO_IA Set to 1 to enable an interrupt when the zero crossing has been missing on Current Channel A for the length of time specified in the ZXTOUT register
+//12 ZXIA Set to 1 to enable an interrupt when the current Channel A zero crossing occurs
+//13 OIA Set to 1 to enable an interrupt when the current Channel A peak has exceeded the overcurrent threshold set in the OILVL register
+//14 ZXTO Set to 1 to enable an interrupt when a zero crossing has been missing on the voltage channel for the length of time specified in the ZXTOUT register
+//15 ZXV Set to 1 to enable an interrupt when the voltage channel zero crossing occurs
+//16 OV Set to 1 to enable an interrupt when the voltage peak has exceeded the overvoltage threshold set in the OVLVL register
+//17 WSMP Set to 1 to enable an interrupt when new waveform data is acquired
+//18 CYCEND Set to 1 to enable an interrupt when it is the end of a line cycle accumulation period
+//19 Sag Set to 1 to enable an interrupt when a sag event has occurred
+//20 Reset This interrupt is always enabled and cannot be disabled
+//21 CRC Set to 1 to enable an interrupt when the checksum has changed
+
+//IRQSTATA Register (Address 0x22D and Address 0x32D) and RSTIRQSTATA Register (Address 0x22E and Address 0x32E)
+//Bits Bit Name Description
+//0 AEHFA Set to 1 when the active energy register is half full (Current Channel A)
+//1 VAREHFA Set to 1 when the reactive energy register is half full (Current Channel A)
+//2 VAEHFA Set to 1 when the apparent energy register is half full (Current Channel A)
+//3 AEOFA Set to 1 when the active energy register has overflowed or underflowed (Current Channel A)
+//4 VAREOFA Set to 1 when the reactive energy register has overflowed or underflowed (Current Channel A)
+//5 VAEOFA Set to 1 when the apparent energy register has overflowed or underflowed (Current Channel A)
+//6 AP_NOLOADA Set to 1 when the active power no-load condition is detected Current Channel A
+//7 VAR_NOLOADA Set to 1 when the reactive power no-load condition is detected Current Channel A
+//8 VA_NOLOADA Set to 1 when the apparent power no-load condition is detected Current Channel A
+//9 APSIGN_A Set to 1 when the sign of active energy has changed (Current Channel A)
+//10 VARSIGN_A Set to 1 when the sign of reactive energy has changed (Current Channel A)
+//11 ZXTO_IA Set to 1 when a zero crossing has been missing on Current Channel A for the length of time specified in the ZXTOUT register
+//12 ZXIA Set to 1 when a current Channel A zero crossing is detected
+//13 OIA Set to 1 when the current Channel A peak has exceeded the overcurrent threshold set in the OILVL register
+//14 ZXTO Set to 1 when a zero crossing has been missing on the voltage channel for the length of time specified in the ZXTOUT register
+//15 ZXV Set to 1 when the voltage channel zero crossing is detected
+//16 OV Set to 1 when the voltage peak has exceeded the overvoltage threshold set in the OVLVL register
+//17 WSMP Set to 1 when new waveform data is acquired
+//18 CYCEND Set to 1 at the end of a line cycle accumulation period
+//19 Sag Set to 1 when a sag event has occurred
+//20 Reset Set to 1 at the end of a software or hardware reset
+//21 CRC Set to 1 when the checksum has changed
+
+//IRQENB Register (Address 0x22F and Address 0x32F)
+//Bits Bit Name Description
+//0 AEHFB Set to 1 to enable an interrupt when the active energy is half full (Current Channel B)
+//1 VAREHFB Set to 1 to enable an interrupt when the reactive energy is half full (Current Channel B)
+//2 VAEHFB Set to 1 to enable an interrupt when the apparent energy is half full (Current Channel B)
+//3 AEOFB Set to 1 to enable an interrupt when the active energy has overflowed or underflowed (Current Channel B)
+//4 VAREOFB Set to 1 to enable an interrupt when the reactive energy has overflowed or underflowed (Current Channel B)
+//5 VAEOFB Set to 1 to enable an interrupt when the apparent energy has overflowed or underflowed (Current Channel B)
+//6 AP_NOLOADB Set to 1 to enable an interrupt when the active power no-load detection on Current Channel B occurs
+//7 VAR_NOLOADB Set to 1 to enable an interrupt when the reactive power no-load detection on Current Channel B occurs
+//8 VA_NOLOADB Set to 1 to enable an interrupt when the apparent power no-load detection on Current Channel B occurs
+//9 APSIGN_B Set to 1 to enable an interrupt when the sign of active energy has changed (Current Channel B)
+//10 VARSIGN_B Set to 1 to enable an interrupt when the sign of reactive energy has changed (Current Channel B)
+//11 ZXTO_IB Set to 1 to enable an interrupt when a zero crossing has been missing on Current Channel B for the length of time specified in the ZXTOUT register
+//12 ZXIB Set to 1 to enable an interrupt when the current Channel B zero crossing occurs
+//13 OIB Set to 1 to enable an interrupt when the current Channel B peak has exceeded the overcurrent threshold set in the OILVL register
+
+//IRQSTATB Register (Address 0x230 and Address 0x330) and RSTIRQSTATB Register (Address 0x231 and Address 0x331)
+//Bits Bit Name Description
+//0 AEHFB Set to 1 when the active energy register is half full (Current Channel B)
+//1 VAREHFB Set to 1 when the reactive energy register is half full (Current Channel B)
+//2 VAEHFB Set to 1 when the apparent energy register is half full (Current Channel B)
+//3 AEOFB Set to 1 when the active energy register has overflowed or underflowed (Current Channel B)
+//4 VAREOFB Set to 1 when the reactive energy register has overflowed or underflowed (Current Channel B)
+//5 VAEOFB Set to 1 when the apparent energy register has overflowed or underflowed (Current Channel B)
+//6 AP_NOLOADB Set to 1 when the active power no-load condition is detected on Current Channel B
+//7 VAR_NOLOADB Set to 1 when the reactive power no-load condition is detected on Current Channel B
+//8 VA_NOLOADB Set to 1 when the apparent power no-load condition is detected on Current Channel B
+//9 APSIGN_B Set to 1 when the sign of active energy has changed (Current Channel B)
+//10 VARSIGN_B Set to 1 when the sign of reactive energy has changed (Current Channel B)
+//11 ZXTO_IB Set to 1 when a zero crossing has been missing on Current Channel B for the length of time specified in the ZXTOUT register
+//12 ZXIB Set to 1 when a current Channel B zero crossing is obtained
+//13 OIB Set to 1 when current Channel B peak has exceeded the overcurrent threshold set in the OILVL register
+
+
 //*******************************************************************************************
 
 
